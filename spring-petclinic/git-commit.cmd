45bd46d3a6f16c241376b10e7916858ab76256bd
praskr <prashanth.kr@capgemini.com>
2015-10-12 18:23:52 +0530

    test commit2

--- spring-petclinic/git-commit.cmd
+++ spring-petclinic/git-commit.cmd
@@ -4,5 +4,5 @@ git status
 set /p commitComment=Enter the Commit Comment:%=%
 git commit -m "%commitComment%"
 git status
+git remote set-url origin https://praskr:test1234@github.com/praskr/awsdemo.git
 git push origin master
-sudo git push --repo https://praskr:test1234@github.com/praskr/awsdemo.git
