4d04c8515d6fd8785b8dddcba6da80143f648c36
praskr <prashanth.kr@capgemini.com>
2015-10-12 18:20:57 +0530

    Test commit1

--- spring-petclinic/git-commit.cmd
+++ spring-petclinic/git-commit.cmd
@@ -5,3 +5,4 @@ set /p commitComment=Enter the Commit Comment:%=%
 git commit -m "%commitComment%"
 git status
 git push origin master
+sudo git push --repo https://praskr:test1234@github.com/praskr/awsdemo.git
